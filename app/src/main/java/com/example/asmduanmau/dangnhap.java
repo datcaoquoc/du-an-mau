c3fe5022f2026142d49edc61de2c6a9b1198df3b
datcaoquoc <quocdatcao3@gmail.com>
2020-10-03 13:47:31 +0700

    man hinh chào, màn hình đăng nhập và đăng kí

--- app/src/main/java/com/example/asmduanmau/dangnhap.java
+++ app/src/main/java/com/example/asmduanmau/dangnhap.java
@@ -2,13 +2,27 @@ package com.example.asmduanmau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class dangnhap extends AppCompatActivity {
-
+    Button btndangnhap;
+    TextView tvchuyendangki;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangnhap);
+        btndangnhap = findViewById(R.id.btndangnhap);
+        tvchuyendangki = findViewById(R.id.tvchuyendangki);
+        tvchuyendangki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dangnhap.this,dangki.class);
+                startActivity(intent);
+            }
+        });
     }
 }
